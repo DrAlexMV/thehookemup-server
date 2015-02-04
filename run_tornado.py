e733bbdb093bdeb6a0d2c4abc7c4144aaa4e5e03
Nicholas Sundin <nsundin2@gmail.com>
2015-02-04 15:15:15 -0600

    Tornado  now listens for changes to the code and restarts. Must be run in development mode: python run_tornado.py development

--- run_tornado.py
+++ run_tornado.py
@@ -1,9 +1,16 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado import autoreload
 from project import app
+import sys
 
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(5000)
-IOLoop.instance().start()
+ioloop = IOLoop.instance()
 
+if len(sys.argv) > 1 and sys.argv[1] == 'development':
+    print 'The server is running in development mode.'
+    autoreload.start(ioloop)
+
+ioloop.start()

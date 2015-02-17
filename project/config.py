3aa5b6cdb4dd48ed11256b552fa6716aa334f688
Nicholas Sundin <nsundin2@gmail.com>
2015-02-17 15:45:10 -0600

    Allow listen port and database name to be changed by env vars.

--- project/config.py
+++ project/config.py
@@ -5,8 +5,11 @@ config = {}
 system_mongo_host = os.environ.get('MONGODB_PORT_27017_TCP_ADDR')
 system_elastic_host = os.environ.get('ELASTIC_PORT_9300_TCP_ADDR')
 
+system_api_port = os.environ.get('API_PORT')
+system_database_name = os.environ.get('DB_NAME')
+
 config['HOST'] = ''
-config['PORT'] = 5000
+config['PORT'] = system_api_port if system_api_port else 5000
 config['MONGODB_HOST'] = system_mongo_host if system_mongo_host else 'localhost'
 config['MONGODB_PORT'] = 27017
 config['ELASTIC_HOST'] = system_elastic_host if system_elastic_host else 'localhost'
@@ -14,7 +17,7 @@ config['ELASTIC_PORT'] = 9200
 config['ACCEPTED_ORIGINS'] = [r'https?://\w*\.?founderati.io', 'http://localhost:3000']
 config['DEBUG'] = False
 config['SECRET_KEY'] = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-config['DATABASE_NAME'] = 'thehookemup'
+config['DATABASE_NAME'] = system_database_name if system_database_name else 'thehookemup'
 config['ROUTE_PREPEND'] = '/api/v1'
 config['FB_SIGNIN_APPID'] = '744792018972866'
 config['FB_SIGNIN_APPSECRET'] = 'd12045378fed762da38f9a882f727828'
--- run_tornado.py
+++ run_tornado.py
@@ -2,11 +2,11 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado import autoreload
-from project import app
+from project import app, config
 import sys
 
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
+http_server.listen(config['PORT'])
 ioloop = IOLoop.instance()
 
 if len(sys.argv) > 1 and sys.argv[1] == 'development':

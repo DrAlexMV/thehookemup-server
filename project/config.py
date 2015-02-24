77bfc70a16241e7ecb8ddb713e1d7312bf0fbdbc
Nicholas Sundin <nsundin2@gmail.com>
2015-02-24 14:50:46 -0600

    Added configuration file support. closes #2

--- project/config.py
+++ project/config.py
@@ -1,25 +1,36 @@
 import os
+import json
 
 config = {}
 
-system_mongo_host = os.environ.get('MONGODB_PORT_27017_TCP_ADDR')
-system_elastic_host = os.environ.get('ELASTIC_PORT_9300_TCP_ADDR')
-
-system_api_port = os.environ.get('API_PORT')
-system_database_name = os.environ.get('DB_NAME')
-
+# Defaults
 config['HOST'] = ''
-config['PORT'] = system_api_port if system_api_port else 5000
-config['MONGODB_HOST'] = system_mongo_host if system_mongo_host else 'localhost'
+config['PORT'] = 5000
+config['MONGODB_HOST'] = 'localhost'
 config['MONGODB_PORT'] = 27017
-config['ELASTIC_HOST'] = system_elastic_host if system_elastic_host else 'localhost'
+config['ELASTIC_HOST'] = 'localhost'
 config['ELASTIC_PORT'] = 9200
-config['ACCEPTED_ORIGINS'] = ['http://localhost:3000', 'http://beta.founderati.io', 'http://3ds.founderati.io']
+config['ACCEPTED_ORIGINS'] = ['http://localhost:3000']
 config['DEBUG'] = False
 config['SECRET_KEY'] = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-config['DATABASE_NAME'] = system_database_name if system_database_name else 'thehookemup'
+config['DATABASE_NAME'] = 'thehookemup'
 config['ROUTE_PREPEND'] = '/api/v1'
 config['FB_SIGNIN_APPID'] = '744792018972866'
 config['FB_SIGNIN_APPSECRET'] = 'd12045378fed762da38f9a882f727828'
 
 config['NEW_USER_INVITE_NUM'] = 3
+
+# Load config file if there is one
+config_file = os.environ.get('CONFIG_FILE')
+if config_file:
+    print "Loading config from %s" % config_file
+    file_customization = json.load(open(config_file))
+    config.update(file_customization)
+
+# Load environment variable settings
+# List of environment variable aliases mapped to their config settings
+environment_vars_configs = {'API_PORT': 'PORT', 'DB_NAME': 'DATABASE_NAME', 'MONGODB_PORT_27017_TCP_ADDR': 'MONGODB_HOST', 'ELASTIC_PORT_9300_TCP_ADDR': 'ELASTIC_HOST'}
+for env_key, config_name in environment_vars_configs.items():
+    env_value = os.environ.get(env_key)
+    if env_value is not None:
+        config[config_name] = env_value

d5b55d82f3c99323f8a2db102e3781942da9df5f
Nicholas Sundin <nsundin2@gmail.com>
2015-03-20 19:43:34 +0100

    Allow image upload from remote URI.

--- project/api/images.py
+++ project/api/images.py
@@ -1,20 +1,17 @@
-from flask import request, Blueprint, request, jsonify, make_response, current_app
-from flask.ext.login import login_user, login_required, logout_user, abort
-from project import utils
+from flask import Blueprint, request, jsonify, make_response, current_app
 from project.services.auth import Auth
-from flask.ext.api import FlaskAPI, exceptions
 from flask.ext.api.status import *
-from bson.objectid import ObjectId
 from wand.image import Image
-from werkzeug import secure_filename
 from models.user import getUserID
 from models.image import create_image, find_image_by_id
+import urllib2
 
 ALLOWED_EXTENSIONS = set(['bmp', 'png', 'jpg', 'jpeg', 'gif'])
 
-PROFILE_SIZE = 280 #280 x 280px
+PROFILE_SIZE = 280  # 280 x 280px
 PROFILE_FORMAT = 'jpeg'
 PROFILE_QUALITY = 85
+
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -24,19 +21,21 @@ blueprint = Blueprint(
     'images', __name__
 )
 
+
 def crop_image(image):
     crop = image.clone()
     square_size = min(crop.height, crop.width)
-    crop.transform('%ix%i'%(square_size, square_size),
-        '%ix%i'%(PROFILE_SIZE, PROFILE_SIZE))
+    crop.transform('%ix%i' % (square_size, square_size), '%ix%i' % (PROFILE_SIZE, PROFILE_SIZE))
     crop.format = PROFILE_FORMAT
     crop.compression_quality = PROFILE_QUALITY
     return crop
 
+
 # Fix POST hang by giving preflight the OPTIONS OK it needs. Hacky but works.
-@blueprint.route('/images',  methods=['OPTIONS'])
+@blueprint.route('/images', methods=['OPTIONS'])
 def handle_preflight():
     return current_app.make_default_options_response()
+
 
 @blueprint.route('/images', methods=['POST'])
 @Auth.require(Auth.USER)
@@ -45,7 +44,6 @@ def upload_image():
     user_id = getUserID('me')
     file = request.files['file']
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
         try:
             image_blob = crop_image(Image(file=file)).make_blob()
             image_id = create_image(image_blob, user_id)
@@ -54,7 +52,28 @@ def upload_image():
             request_error = str(e)
     return jsonify(error=request_error), HTTP_400_BAD_REQUEST
 
-@blueprint.route('/images/<imageid>.'+PROFILE_FORMAT, methods=['GET'])
+
+@blueprint.route('/images/upload-from-uri', methods=['POST'])
+@Auth.require(Auth.USER)
+def upload_image_from_uri():
+    user_id = getUserID('me')
+    file_location = request.get_json().get('uri')
+
+    # Consider Tornado's async retrival here
+    try:
+        image_info = urllib2.urlopen(file_location)
+    except:
+        return jsonify(error='Bad URI'), HTTP_400_BAD_REQUEST
+
+    try:
+        image_blob = crop_image(Image(file=image_info)).make_blob()
+        image_id = create_image(image_blob, user_id)
+        return jsonify(error=None, imageID=str(image_id) + '.' + PROFILE_FORMAT)
+    except Exception as e:
+        return jsonify(error=str(e)), HTTP_400_BAD_REQUEST
+
+
+@blueprint.route('/images/<imageid>.' + PROFILE_FORMAT, methods=['GET'])
 @Auth.require(Auth.USER)
 def get_image(imageid):
     entry = find_image_by_id(imageid)
@@ -65,14 +84,15 @@ def get_image(imageid):
     response.headers['Content-Type'] = 'image/jpeg'
     return response
 
-@blueprint.route('/images/<imageid>.'+PROFILE_FORMAT, methods=['DELETE'])
+
+@blueprint.route('/images/<imageid>.' + PROFILE_FORMAT, methods=['DELETE'])
 @Auth.require(Auth.USER)
 def delete_image(imageid):
     entry = find_image_by_id(imageid)
     if entry is None:
         return '', HTTP_404_NOT_FOUND
 
-    if str(entry['owner']) != str(getUserID('me')): # TODO: Add admin override
+    if str(entry['owner']) != str(getUserID('me')):  # TODO: Add admin override
         return '', HTTP_401_UNAUTHORIZED
 
     entry.delete()
